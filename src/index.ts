8a6842e513cded2fcaaa69f6a404c422da2bd56f
Vinicius Alencar <viniciusalencardev@gmail.com>
2023-01-15 21:27:01 -0300

    Connection with server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
 import amqplib from 'amqplib';
+
+amqplib.connect('amqp://localhost', (err0: any, connection: any) => {
+    if (err0) {
+        throw err0;
+    }
+
+    connection.createChannel(function (err1: any, channel: any) {
+        if (err1) {
+            throw err1;
+        }
+
+        var queue = 'example';
+        var msg = 'New message';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log("Message Sent:", msg);
+
+        channel.consume(queue, (msg: any) => {
+            console.log("Message Receive:", msg.content.toString());
+        }, {
+            noAck: true
+        });
+    });
+});
